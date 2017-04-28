d971e7b964a63edc3f813edbb9cf7117c0989ce7
namnvbph04220 <namnvbph04220@fpt.edu.vn>
2017-04-28 16:18:39 +0700

    BangLoaiSp
    nam

---
+++ Ass/Ass/Ass/Ass/BangLoaiSanPham.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ass.DataSetTableAdapters;
+using System.Data;
+
+namespace Ass
+{
+    public partial class BangLoaiSanPham : Form
+    {
+        LoaiSanPhamTableAdapter _adapterLoaiSP = new LoaiSanPhamTableAdapter();
+        ItemLoaiSanPham _itemLoaiSP = new ItemLoaiSanPham("");
+       
+        public BangLoaiSanPham()
+        {
+            InitializeComponent();
+            DataTable _dataLoaisp = _adapterLoaiSP.GetData();
+            showLoaiSP.DataSource = _dataLoaisp.DefaultView;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void themLoaiSp_Click(object sender, EventArgs e)
+        {
+            _itemLoaiSP = new ItemLoaiSanPham("");
+            _itemLoaiSP.ShowDialog();
+            if (_itemLoaiSP.ChekKq == true) {
+                DataTable _data = _adapterLoaiSP.GetData();
+                showLoaiSP.DataSource = _data.DefaultView;
+            }
+        }
+
+        private void SuaLoaiSP_Click(object sender, EventArgs e)
+        {
+            string maLoaiSp = showLoaiSP.CurrentRow.Cells["MaLoaiSP"].Value.ToString();
+            _itemLoaiSP = new ItemLoaiSanPham(maLoaiSp);
+            _itemLoaiSP.ShowDialog();
+            if (_itemLoaiSP.ChekKq == true)
+            {
+                DataTable _data = _adapterLoaiSP.GetData();
+                showLoaiSP.DataSource = _data.DefaultView;
+            }
+        }
+
+        private void XoaLoaiSp_Click(object sender, EventArgs e)
+        {
+            string maloaisp = showLoaiSP.CurrentRow.Cells["MaLoaiSP"].Value.ToString();
+            _itemLoaiSP = new ItemLoaiSanPham(maloaisp);
+            _adapterLoaiSP.XoaLoaiSP(maloaisp);
+            MessageBox.Show("Xóa Thành Công");
+            DataTable _data = _adapterLoaiSP.GetData();
+            showLoaiSP.DataSource = _data.DefaultView;
+        }
+    }
+}
---
+++ Ass/Ass/Ass/Ass/ItemLoaiSanPham.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Ass
+{
+    partial class ItemLoaiSanPham
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtMaLoaiSP = new System.Windows.Forms.TextBox();
+            this.txtTenLoaiSP = new System.Windows.Forms.TextBox();
+            this.txtGhiChu = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ma Loai Sp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ten Loai San Pham";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ghi Chu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Luu Lai";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Lam Lai";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtMaLoaiSP
+            // 
+            this.txtMaLoaiSP.Location = new System.Drawing.Point(220, 27);
+            this.txtMaLoaiSP.Name = "txtMaLoaiSP";
+            this.txtMaLoaiSP.Size = new System.Drawing.Size(287, 20);
+            this.txtMaLoaiSP.TabIndex = 2;
+            // 
+            // txtTenLoaiSP
+            // 
+            this.txtTenLoaiSP.Location = new System.Drawing.Point(220, 77);
+            this.txtTenLoaiSP.Name = "txtTenLoaiSP";
+            this.txtTenLoaiSP.Size = new System.Drawing.Size(287, 20);
+            this.txtTenLoaiSP.TabIndex = 2;
+            // 
+            // txtGhiChu
+            // 
+            this.txtGhiChu.Location = new System.Drawing.Point(220, 132);
+            this.txtGhiChu.Name = "txtGhiChu";
+            this.txtGhiChu.Size = new System.Drawing.Size(287, 20);
+            this.txtGhiChu.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(562, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ItemLoaiSanPham
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 261);
+            this.Controls.Add(this.txtGhiChu);
+            this.Controls.Add(this.txtTenLoaiSP);
+            this.Controls.Add(this.txtMaLoaiSP);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ItemLoaiSanPham";
+            this.Text = "ItemLoaiSanPham";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtMaLoaiSP;
+        private System.Windows.Forms.TextBox txtTenLoaiSP;
+        private System.Windows.Forms.TextBox txtGhiChu;
+        private System.Windows.Forms.Button button3;
+    }
+}
